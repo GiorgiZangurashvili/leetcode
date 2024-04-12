61bf6151ef63c64ecbebda140faebe8d376f62e7
GiorgiZangurashvili <158226767+GiorgiZangurashvili@users.noreply.github.com>
2024-04-12 20:21:03 +0400

    Create 0217-contains-duplicate.java

---
+++ 0217-contains-duplicate.java
@@ -0,0 +1,24 @@
+/*
+    Given int array, return true if any value appears at least twice
+    Ex. nums = [1,2,3,1] -> true, nums = [1,2,3,4] -> false
+
+    If the number has been seen previously, then it has a duplicate. Otherwise, insert it into the hash set.
+
+    Time: O(n)
+    Space: O(n)
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
