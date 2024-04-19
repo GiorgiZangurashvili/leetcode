2f3446702bd4ad763fffd738061c3122b1b2bfe7
GiorgiZangurashvili <158226767+GiorgiZangurashvili@users.noreply.github.com>
2024-04-19 22:24:10 +0400

    Create 0226-invert-binary-tree.java

---
+++ 0226-invert-binary-tree.java
@@ -0,0 +1,35 @@
+/*
+  Given the root of a binary tree, invert the tree, and return its root.
+  
+  Time: O(n)
+  Space: O(h)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left), right = invertTree(root.right);
+        TreeNode temp = left;
+        root.left = right;
+        root.right = temp;
+        return root;
+    }
+}
